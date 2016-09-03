TiftGolofebitke/vega-lite

commit ff1e61e5209ede3dc424c27c3b5226972edfb6d6

Quick Fix for count's number format -- addressing https://github.com/vega/vega-lite/issues/1521

src/compile/common.ts
@@ -1,4 +1,5 @@
 import {BAR, POINT, CIRCLE, SQUARE} from '../mark';
+import {AggregateOp} from '../aggregate';
 import {COLOR, OPACITY} from '../channel';
 import {Config} from '../config';
 import {FieldDef, field, OrderChannelDef} from '../fielddef';
@@ -119,8 +120,15 @@ export function applyMarkConfig(marksProperties, model: UnitModel, propsList: st
 export function numberFormat(fieldDef: FieldDef, format: string, config: Config) {
   if (fieldDef.type === QUANTITATIVE && !fieldDef.bin) {
     // add number format for quantitative type only
+
+    if (format) {
+      return format;
+    } else if (fieldDef.aggregate === AggregateOp.COUNT) {
+      // FIXME: need a more holistic way to deal with this.
+      return 'd';
+    }
     // TODO: need to make this work correctly for numeric ordinal / nominal type
-    return format || config.numberFormat;
+    return config.numberFormat;
   }
   return undefined;
 }

commit 4ed56465c7b8f1fb22648a6b6b0d476d5280308b

Only use d as count format for text channel
Addressing problem introduced in ff1e61e5209ede3dc424c27c3b5226972edfb6d6

src/compile/axis.ts
@@ -129,7 +129,7 @@ export function parseAxis(channel: Channel, model: Model): VgAxis {
 }
 
 export function format(model: Model, channel: Channel) {
-  return numberFormat(model.fieldDef(channel), model.axis(channel).format, model.config());
+  return numberFormat(model.fieldDef(channel), model.axis(channel).format, model.config(), channel);
 }
 
 export function offset(model: Model, channel: Channel) {

src/compile/common.ts
@@ -1,6 +1,6 @@
 import {BAR, POINT, CIRCLE, SQUARE} from '../mark';
 import {AggregateOp} from '../aggregate';
-import {COLOR, OPACITY} from '../channel';
+import {COLOR, OPACITY, TEXT, Channel} from '../channel';
 import {Config} from '../config';
 import {FieldDef, field, OrderChannelDef} from '../fielddef';
 import {SortOrder} from '../sort';
@@ -117,13 +117,13 @@ export function applyMarkConfig(marksProperties, model: UnitModel, propsList: st
  *
  * @param format explicitly specified format
  */
-export function numberFormat(fieldDef: FieldDef, format: string, config: Config) {
+export function numberFormat(fieldDef: FieldDef, format: string, config: Config, channel: Channel) {
   if (fieldDef.type === QUANTITATIVE && !fieldDef.bin) {
     // add number format for quantitative type only
 
     if (format) {
       return format;
-    } else if (fieldDef.aggregate === AggregateOp.COUNT) {
+    } else if (fieldDef.aggregate === AggregateOp.COUNT && channel === TEXT) {
       // FIXME: need a more holistic way to deal with this.
       return 'd';
     }

src/compile/legend.ts
@@ -55,7 +55,7 @@ export function parseLegend(model: UnitModel, channel: Channel): VgLegend {
 
   // 1.1 Add properties with special rules
   def.title = title(legend, fieldDef, config);
-  const format = numberFormat(fieldDef, legend.format, config);
+  const format = numberFormat(fieldDef, legend.format, config, channel);
   if (format) {
     def.format = format;
   }

src/compile/mark/text.ts
@@ -112,7 +112,7 @@ export namespace text {
     if (textFieldDef) {
       if (textFieldDef.field) {
         if (QUANTITATIVE === textFieldDef.type) {
-          const format = numberFormat(textFieldDef, config.mark.format, config);
+          const format = numberFormat(textFieldDef, config.mark.format, config, TEXT);
 
           const filter = 'number' + ( format ? ':\'' + format + '\'' : '');
           return {

test/compile/common.test.ts
@@ -2,6 +2,7 @@
 
 import {assert} from 'chai';
 import {parseUnitModel} from '../util';
+import {X} from '../../src/channel';
 import {defaultConfig} from '../../src/config';
 import {TimeUnit} from '../../src/timeunit';
 import {field, FieldDef} from '../../src/fielddef';
@@ -42,24 +43,24 @@ describe('Common', function() {
 
   describe('numberFormat()', function() {
     it('should use number format for quantitative scale', function() {
-      assert.equal(numberFormat({field: 'a', type: QUANTITATIVE}, undefined, {numberFormat: 'd'}), 'd');
+      assert.equal(numberFormat({field: 'a', type: QUANTITATIVE}, undefined, {numberFormat: 'd'}, X), 'd');
     });
 
     it('should support empty number format', function() {
-      assert.equal(numberFormat({field: 'a', type: QUANTITATIVE}, undefined, {numberFormat: ''}), '');
+      assert.equal(numberFormat({field: 'a', type: QUANTITATIVE}, undefined, {numberFormat: ''}, X), '');
     });
 
     it('should use format if provided', function() {
-      assert.equal(numberFormat({field: 'a', type: QUANTITATIVE}, 'a', 'd'), 'a');
+      assert.equal(numberFormat({field: 'a', type: QUANTITATIVE}, 'a', 'd', X), 'a');
     });
 
     it('should not use number format for binned quantitative scale', function() {
-      assert.equal(numberFormat({bin: true, field: 'a', type: QUANTITATIVE}, undefined, 'd'), undefined);
+      assert.equal(numberFormat({bin: true, field: 'a', type: QUANTITATIVE}, undefined, 'd', X), undefined);
     });
 
     it('should not use number format for non-quantitative scale', function() {
       for (let type of [TEMPORAL, NOMINAL, ORDINAL]) {
-        assert.equal(numberFormat({bin: true, field: 'a', type: type}, undefined, 'd'), undefined);
+        assert.equal(numberFormat({bin: true, field: 'a', type: type}, undefined, 'd', X), undefined);
       }
     });
   });